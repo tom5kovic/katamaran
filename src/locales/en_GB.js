8839a5d635b105930402ef74008deb713eb36062
Tomislav Petkovic <t.petkovic@vegait.rs>
2023-10-09 12:12:59 +0200

    Move text to locales.

--- src/locales/en_GB.js
+++ src/locales/en_GB.js
@@ -1,5 +1,24 @@
 export default {
   'contact': 'Contact',
   'tours': 'Tours',
-  'gallery': 'Gallery'
+  'gallery': 'Gallery',
+  'welcome-to-river': 'Welcome to River Catamaran Adventures!',
+  'explore-scenic-beauty':
+    "Explore the scenic beauty of our local waterways with our premium river catamaran rentals. At River Catamaran Adventures, we offer an unforgettable experience for water enthusiasts of all levels. Whether you're seeking a leisurely cruise or an exciting aquatic adventure, our top-quality catamarans are perfect for family outings, group gatherings, or a relaxing day on the water with friends.",
+  'why-choose-our': 'Why Choose Our River Catamarans?',
+  'stability-comfort':
+    'Stability & Comfort: Our catamarans are designed for stability, ensuring a smooth and comfortable ride even in choppy waters. You can relax and enjoy the scenic beauty without any worries.',
+  'easy-to-use':
+    "Easy to Use: Our catamarans are user-friendly, making them suitable for beginners and experienced sailors alike. Our team provides comprehensive instructions, ensuring you're ready to set sail with confidence.",
+  'breathtaking-views':
+    'Breathtaking Views: Immerse yourself in the breathtaking views of the river and its surroundings. Spot local wildlife, enjoy picturesque sunsets, and create lasting memories with your loved ones.',
+  'flexible-rental':
+    'Flexible Rental Options: Whether you want to rent a catamaran for a few hours, a whole day, or even a weekend getaway, we offer flexible rental packages tailored to your needs.',
+  'safety-first':
+    'Safety First: Your safety is our top priority. Our catamarans are regularly maintained, and our experienced staff is always available to assist you during your adventure.',
+  'plan-today': 'Plan Your River Adventure Today!',
+  'ready-to-embark':
+    'Ready to embark on a memorable river journey? Browse our fleet, check availability, and book your river catamaran adventure online. If you have any questions or need assistance, feel free to contact our friendly team. Get ready to experience the joy of sailing on the serene waters and create unforgettable moments with River Catamaran Adventures.',
+  'discover-beauty':
+    'Discover the beauty of the river like never before. Book your catamaran adventure today!'
 };
--- src/screens/HomeScreen/HomeScreen.js
+++ src/screens/HomeScreen/HomeScreen.js
@@ -1,65 +1,27 @@
 import React from 'react';
 import './HomeScreen.css';
+import { useTranslation } from 'react-i18next';
 
 export const HomeScreen = () => {
+  const { t } = useTranslation();
   return (
     <div className='centered-wrapper'>
-      <h1>Welcome to River Catamaran Adventures!</h1>
+      <h1>{t('welcome-to-river')}</h1>
+      <p>{t('explore-scenic-beauty')}</p>
+      <h1>{t('why-choose-our')}</h1>
+      <p>{t('stability-comfort')}</p>
       <br />
-      <p>
-        Explore the scenic beauty of our local waterways with our premium river
-        catamaran rentals. At River Catamaran Adventures, we offer an
-        unforgettable experience for water enthusiasts of all levels. Whether
-        you're seeking a leisurely cruise or an exciting aquatic adventure, our
-        top-quality catamarans are perfect for family outings, group gatherings,
-        or a relaxing day on the water with friends.
-      </p>
-      <h1>Why Choose Our River Catamarans?</h1>
+      <p>{t('easy-to-use')}</p>
       <br />
-      <p>
-        Stability & Comfort: Our catamarans are designed for stability, ensuring
-        a smooth and comfortable ride even in choppy waters. You can relax and
-        enjoy the scenic beauty without any worries.
-      </p>
+      <p>{t('breathtaking-views')}</p>
       <br />
-      <p>
-        Easy to Use: Our catamarans are user-friendly, making them suitable for
-        beginners and experienced sailors alike. Our team provides comprehensive
-        instructions, ensuring you're ready to set sail with confidence.
-      </p>
+      <p>{t('flexible-rental')}</p>
       <br />
-      <p>
-        Breathtaking Views: Immerse yourself in the breathtaking views of the
-        river and its surroundings. Spot local wildlife, enjoy picturesque
-        sunsets, and create lasting memories with your loved ones.
-      </p>
+      <p>{t('safety-first')}</p>
+      <h1>{t('plan-today')}</h1>
+      <p>{t('ready-to-embark')}</p>
       <br />
-      <p>
-        Flexible Rental Options: Whether you want to rent a catamaran for a few
-        hours, a whole day, or even a weekend getaway, we offer flexible rental
-        packages tailored to your needs.
-      </p>
-      <br />
-      <p>
-        Safety First: Your safety is our top priority. Our catamarans are
-        regularly maintained, and our experienced staff is always available to
-        assist you during your adventure.
-      </p>
-      <br />
-      <h1>Plan Your River Adventure Today!</h1>
-      <br />
-      <p>
-        Ready to embark on a memorable river journey? Browse our fleet, check
-        availability, and book your river catamaran adventure online. If you
-        have any questions or need assistance, feel free to contact our friendly
-        team. Get ready to experience the joy of sailing on the serene waters
-        and create unforgettable moments with River Catamaran Adventures.
-      </p>
-      <br />
-      <p>
-        Discover the beauty of the river like never before. Book your catamaran
-        adventure today!
-      </p>
+      <p>{t('discover-beauty')}</p>
     </div>
   );
 };
