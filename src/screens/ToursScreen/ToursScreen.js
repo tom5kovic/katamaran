1d45d92cb1ef9bd8d88bb79ebf89d8617c907407
Tomislav Petkovic <t.petkovic@vegait.rs>
2023-10-10 11:43:45 +0200

    Add svg icons and display more info about tour.

--- src/screens/ToursScreen/ToursScreen.js
+++ src/screens/ToursScreen/ToursScreen.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import './ToursScreen.css';
 import { toursData } from '../../data/ToursData';
+import { LocationPin } from '../../components/icons/LocationPin';
+import { Clock } from '../../components/icons/Clock';
+import { DolarSign } from '../../components/icons/DolarSign';
+import { People } from '../../components/icons/People';
 
 export const ToursScreen = () => {
   return (
@@ -9,7 +13,24 @@ export const ToursScreen = () => {
         return (
           <div className='image-wrapper' key={item.id} onClick={() => null}>
             <img src={item.image} className='image' alt={index} />
-            <p>{item.name}</p>
+            <div className='info-container'>
+              <div className='icon-text'>
+                <LocationPin />
+                <p>{item.location}</p>
+              </div>
+              <div className='icon-text'>
+                <Clock />
+                <p>{item.duration}</p>
+              </div>
+              <div className='icon-text'>
+                <DolarSign />
+                <p>{item.price}</p>
+              </div>
+              <div className='icon-text'>
+                <People />
+                <p>{item.numberOfPeopple}</p>
+              </div>
+            </div>
           </div>
         );
       })}
